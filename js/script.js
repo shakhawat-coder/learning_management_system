d9078eff08f0f3652ed567fb250b415e66640e67
shakhawat <shakhawathossen188@gmail.com>
2024-01-08 22:10:46 +0600

    testimony section design for index page

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ new VenoBox({
     delay: 10,
     time: 1000
 });
+
+// $(document).ready(function(){
+//   $('.testimonials_wrapper').slick({
+//       centerMode: true,
+//       centerPadding: '60px',
+//       prevArrow: "",
+//       nextArrow: "",
+//       slidesToShow: 1,
+//       autoplay: false,
+//     });
+// });
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+
+        items: 1,
+        margin: 30,
+        loop:true,
+        dots:false,
+        nav:true,
+        navText: ['<i class="fa-sharp fa-solid fa-arrow-left-long"></i>', '<i class="fa-sharp fa-solid fa-arrow-right-long"></i>'],
+  });
+});
