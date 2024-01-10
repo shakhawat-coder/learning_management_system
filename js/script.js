96625773e0a7d8cade3347aee4cf8f454baba41f
shakhawat <shakhawathossen188@gmail.com>
2024-01-10 23:54:04 +0600

    about page design

--- js/script.js
+++ js/script.js
@@ -18,9 +18,37 @@ new VenoBox({
 //       autoplay: false,
 //     });
 // });
+$('.testimonials_wrapper').on('translated.owl.carousel', function() {
+  $('..testimonials_wrapper .owl-item.active').removeClass('center'); // Remove from previous active item
+  $('.testimonials_wrapper .owl-item.active').addClass('center'); // Add to current active item
+});
+
+$('#aboutpage .testimonials_wrapper .active').addClass('center');
+$(document).ready(function(){
+  $('#aboutpage .testimonials_wrapper').owlCarousel({
+        autoplay:true,
+        items: 3,
+        margin: 30,
+        loop:true,
+        dots:false,
+        nav:true,
+        navText: ['<i class="fa-sharp fa-solid fa-arrow-left-long"></i>', '<i class="fa-sharp fa-solid fa-arrow-right-long"></i>'],
+  });
+});
+$(document).ready(function(){
+  $('.course_slider').owlCarousel({
+        autoplay:true,
+        items: 3,
+        margin: 30,
+        loop:true,
+        dots:false,
+        nav:true,
+        navText: ['<i class="fa-sharp fa-solid fa-arrow-left-long"></i>', '<i class="fa-sharp fa-solid fa-arrow-right-long"></i>'],
+  });
+});
 $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
-
+        autoplay:true,
         items: 1,
         margin: 30,
         loop:true,
@@ -29,6 +57,7 @@ $(document).ready(function(){
         navText: ['<i class="fa-sharp fa-solid fa-arrow-left-long"></i>', '<i class="fa-sharp fa-solid fa-arrow-right-long"></i>'],
   });
 });
+
 
 
 new WOW().init();
